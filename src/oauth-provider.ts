8ee1cca930b03c2549b2bd2b0b092fe0383b9e68
Felix Gladisch <gladisch@sipgate.de>
2018-03-15 17:57:18 +0100

    feat: respond with parsed crm contacts (wip)

--- src/oauth-provider.ts
+++ src/oauth-provider.ts
@@ -1,6 +1,7 @@
-import { OAuth2, Connection } from "jsforce";
+import { Contact } from "clinq-crm-bridge";
 import * as express from "express";
 import { Request, Response } from "express";
+import { Connection, OAuth2 } from "jsforce";
 
 const PORT = 8081;
 
@@ -9,6 +10,70 @@ const {
 	SF_OAUTH_PROVIDER_CLIENT_SECRET: clientSecret,
 	SF_OAUTH_PROVIDER_REDIRECT_URI: redirectUri
 } = process.env;
+
+interface PhoneNumberType {
+	property: string;
+	label: string;
+}
+
+interface PhoneNumber {
+	label: string;
+	phoneNumber: string;
+}
+
+const PhoneNumberTypes: PhoneNumberType[] = [
+	{
+		property: "Phone",
+		label: "Work"
+	},
+	{
+		property: "MobilePhone",
+		label: "Mobile"
+	},
+	{
+		property: "HomePhone",
+		label: "Home"
+	}
+];
+
+const contactHasPhoneNumber = contact =>
+	PhoneNumberTypes.some(type => contact[type.property]);
+
+function convertToCrmContact(contact: any): Contact {
+	const phoneNumbers = PhoneNumberTypes.filter(
+		type => contact[type.property]
+	).map(
+		type =>
+			<PhoneNumber>{
+				label: type.label,
+				phoneNumber: contact[type.property]
+			}
+	)
+	return {
+		name: contact.Name,
+		phoneNumbers
+	};
+}
+
+const redirectToAuthUrl = oauth2 => (req: Request, res: Response) => {
+	res.redirect(oauth2.getAuthorizationUrl({ scope: "api" }));
+}
+
+const handleAuthCallback = oauth2 => async (req: Request, res: Response) => {
+	try {
+		const conn = new Connection({ oauth2 });
+		const { code } = req.query;
+		await conn.authorize(code);
+		const contacts = await conn.sobject("Contact").select("*");
+		const parsedContacts: Contact[] = contacts
+			.filter(contactHasPhoneNumber)
+			.map(convertToCrmContact);
+		res.send(parsedContacts);
+	} catch (error) {
+		console.log("Could not get access token", error.message);
+		res.send("failure");
+	}
+}
 
 export const startOAuthProvider = () => {
 	if (!clientId || !clientSecret || !redirectUri) {
@@ -23,22 +88,9 @@ export const startOAuthProvider = () => {
 
 	const app: express.Application = express();
 
-	app.get("/oauth2/auth", (req: Request, res: Response) => {
-		res.redirect(oauth2.getAuthorizationUrl({ scope: "api" }));
-	});
+	app.get("/oauth2/auth", redirectToAuthUrl(oauth2));
 
-	app.get("/oauth2/callback", async (req: Request, res: Response) => {
-		try {
-			const conn = new Connection({ oauth2 });
-			const { code } = req.query;
-			await conn.authorize(code);
-			console.log("Received access token", conn.accessToken);
-			res.send("success");
-		} catch (error) {
-			console.log("Could not get access token", error.message);
-			res.send("failure");
-		}
-	});
+	app.get("/oauth2/callback", handleAuthCallback(oauth2));
 
 	app.listen(PORT, () => {
 		console.log("OAuth provider listening on port", PORT);
