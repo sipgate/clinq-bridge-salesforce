3599663518d9a3b863ec6a24be21aaf4e3128932
Felix A. Gladisch <felix@felixgladisch.de>
2018-11-12 15:59:55 +0100

    feat: unauthorized

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Adapter, Config, Contact, start } from "@clinq/bridge";
+import { Adapter, Config, Contact, start, unauthorized } from "@clinq/bridge";
 import { Request } from "express";
 import { Connection, OAuth2, OAuth2Options } from "jsforce";
 
@@ -7,28 +7,36 @@ import { contactHasPhoneNumber, convertSalesforceContact, parseEnvironment } fro
 
 const oauth2Options: OAuth2Options = parseEnvironment();
 const oauth2: OAuth2 = new OAuth2(oauth2Options);
+const ANONYMIZED_KEY_CHARACTERS = 8;
 
 class SalesforceAdapter implements Adapter {
 	public async getContacts({ apiKey, apiUrl }: Config): Promise<Contact[]> {
-		const [accessToken, refreshToken] = apiKey.split(":");
-		const connection: Connection = new Connection({
-			accessToken,
-			instanceUrl: apiUrl,
-			oauth2,
-			refreshToken
-		});
-		const contacts: SalesforceContact[] = await connection.sobject("Contact").select("*");
-		const anonymizedKey = `***${apiKey.substr(apiKey.length - 5, apiKey.length)}`;
-		console.log(
-			`Found ${contacts.length} Salesforce contacts for API key ${anonymizedKey} on ${apiUrl}`
-		);
-		const parsedContacts: Contact[] = contacts
-			.filter(contactHasPhoneNumber)
-			.map(convertSalesforceContact(apiUrl));
-		console.log(
-			`Parsed ${parsedContacts.length} contacts for API key ${anonymizedKey} on ${apiUrl}`
-		);
-		return parsedContacts;
+		try {
+			const [accessToken, refreshToken] = apiKey.split(":");
+			const connection: Connection = new Connection({
+				accessToken,
+				instanceUrl: apiUrl,
+				oauth2,
+				refreshToken
+			});
+			const contacts: SalesforceContact[] = await connection.sobject("Contact").select("*");
+			const anonymizedKey = `***${refreshToken.substr(
+				refreshToken.length - ANONYMIZED_KEY_CHARACTERS,
+				refreshToken.length
+			)}`;
+			console.log(
+				`Found ${contacts.length} Salesforce contacts for API key ${anonymizedKey} on ${apiUrl}`
+			);
+			const parsedContacts: Contact[] = contacts
+				.filter(contactHasPhoneNumber)
+				.map(convertSalesforceContact(apiUrl));
+			console.log(
+				`Parsed ${parsedContacts.length} contacts for API key ${anonymizedKey} on ${apiUrl}`
+			);
+			return parsedContacts;
+		} catch {
+			unauthorized();
+		}
 	}
 
 	public getOAuth2RedirectUrl(): Promise<string> {
